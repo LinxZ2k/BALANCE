02dff17d3b2743bc73f9d7c6fa333e44bc203a2e
Linx <105833005+LinxZ2k@users.noreply.github.com>
2023-01-19 00:48:03 +0630

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,5 @@
+hamburger = document.querySelector(".menu")
+        hamburger.onclick = function(){
+            navBar = document.querySelector(".nav_bar")
+            navBar.classList.toggle("touch")
+        }
